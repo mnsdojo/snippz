599da2a2787e02c621e0ca06bf1a68660d4edb14
notageek-guy <manishkumar97430@gmail.com>
2024-02-16 20:04:17 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Herosection from "@/components/Herosection";
+import { ModeToggle } from "@/components/ui/ThemeToggle";
+import Link from "next/link";
+
+async function Page() {
+  return (
+    <div className="relative ">
+      <Herosection />
+    </div>
+  );
+}
+
+export default Page;
